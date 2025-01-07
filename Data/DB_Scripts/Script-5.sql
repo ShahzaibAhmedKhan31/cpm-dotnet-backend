e19e577266a866e4f595edbda310ab8e7a083824
daniyalimam5 <imamdaniyal0@gmail.com>
2025-01-07 12:40:57 +0500

    updated db scripts

--- Data/DB_Scripts/Script-5.sql
+++ Data/DB_Scripts/Script-5.sql
@@ -1,21 +1,21 @@
 ```sql
-
+ 
 -- Create tenant table
 CREATE TABLE tenant (
     tenantid VARCHAR(255) PRIMARY KEY,
     adminid VARCHAR(255),
     adminemail VARCHAR(255),
     ad_url VARCHAR(255);
-
+ 
 );
-
+ 
 -- Create dept table
 CREATE TABLE dept (
     deptid VARCHAR(255) PRIMARY KEY,
     hodid VARCHAR(255),
     tenantid VARCHAR(255)
 );
-
+ 
 -- Create employee table
 CREATE TABLE employee (
     empid VARCHAR(255) PRIMARY KEY,
@@ -28,23 +28,23 @@ CREATE TABLE employee (
     deptid VARCHAR(255),
     active VARCHAR(255)
 );
-
-
-
+ 
+ 
+ 
 -- Create roles table
 CREATE TABLE roles (
     roleid VARCHAR(255) PRIMARY KEY,
     tenantid VARCHAR(255),
     deptid VARCHAR(255)
 );
-
+ 
 -- Create permissions table
 CREATE TABLE permissions (
     permid VARCHAR(255) PRIMARY KEY,
     roleid VARCHAR(255),
     permissionname VARCHAR(255)
 );
-
+ 
 -- Create datasource table
 CREATE TABLE datasource (
     sourceid VARCHAR(255) PRIMARY KEY,
@@ -52,7 +52,7 @@ CREATE TABLE datasource (
     sourcelink VARCHAR(255),
     comments VARCHAR(255)
 );
-
+ 
 -- Create kra table
 CREATE TABLE kra (
     kraid VARCHAR(255) PRIMARY KEY,
@@ -61,31 +61,35 @@ CREATE TABLE kra (
     kratype VARCHAR(255),
     reportedto VARCHAR(255)
 );
-
+ 
 -- Create tasks table
-CREATE TABLE tasks (
+CREATE TABLE monthlytask (
     taskid VARCHAR(255) PRIMARY KEY,
     tasktype VARCHAR(255),
     kraid VARCHAR(255),
     date DATE
 );
-
+drop table montlytask;
+ 
 -- Create task_scores table
-CREATE TABLE task_scores (
-    scoreid VARCHAR(255) PRIMARY KEY,
-    taskid VARCHAR(255),
-    superrating INT,
-    automatedrating INT,
-    comments VARCHAR(255)
+CREATE TABLE taskscores (
+    ScoreId SERIAL PRIMARY KEY,
+    TaskId VARCHAR(255),
+    SuperRating FLOAT,
+    SelfRating FLOAT,
+    AutomatedRating FLOAT,
+    Comments TEXT
 );
-
+ 
+drop table taskscores;
+ 
 -- Create kpi_details table
 CREATE TABLE kpi_details (
     kpi_id VARCHAR(255) PRIMARY KEY,
     kpi_name VARCHAR(255),
     kpi_score FLOAT
 );
-
+ 
 -- Create kpi_criteria table
 CREATE TABLE kpi_criteria (
     criteria_id INT PRIMARY KEY,
@@ -95,7 +99,7 @@ CREATE TABLE kpi_criteria (
     no_of_comments INT,
     kpi_id VARCHAR(255)
 );
-
+ 
 -- Create competencies table
 CREATE TABLE competencies (
     compid VARCHAR(255) PRIMARY KEY,
@@ -103,14 +107,14 @@ CREATE TABLE competencies (
     department VARCHAR(255),
     tenant VARCHAR(255)
 );
-
+ 
 -- Create competency_score table
 CREATE TABLE competency_score (
     compscoreid VARCHAR(255) PRIMARY KEY,
     compid VARCHAR(255),
     taskid VARCHAR(255)
 );
-
-
-
+ 
+ 
+ 
 ```
